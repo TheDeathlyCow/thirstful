df58a660705cd2022d69dade6647b05f8df5949a
TheDeathlyCow <thedeathlycow@gmail.com>
2025-01-12 04:57:33 -1000

    import a template thirstful

---
+++ src/client/java/com/thedeathlycow/thirstful/ThirstfulClient.java
@@ -0,0 +1,10 @@
+package com.thedeathlycow.thirstful;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThirstfulClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
