3e8625661fc077c8565ee4a4c587f429f4430e8d
TheDeathlyCow <53499406+TheDeathlyCow@users.noreply.github.com>
2025-09-30 23:26:35 +1300

    add block codec

--- src/main/java/com/thedeathlycow/thirstful/block/MeatStillBlock.java
+++ src/main/java/com/thedeathlycow/thirstful/block/MeatStillBlock.java
@@ -1,6 +1,7 @@
 package com.thedeathlycow.thirstful.block;
 
 import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.thedeathlycow.thirstful.block.entity.MeatStillBlockEntity;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
@@ -8,13 +9,20 @@ import net.minecraft.util.math.BlockPos;
 import org.jetbrains.annotations.Nullable;
 
 public class MeatStillBlock extends BlockWithEntity {
+    public static final MapCodec<MeatStillBlock> CODEC = RecordCodecBuilder.mapCodec(
+            instance ->
+                    instance.group(
+                            createSettingsCodec()
+                    ).apply(instance, MeatStillBlock::new)
+    );
+
     public MeatStillBlock(Settings settings) {
         super(settings);
     }
 
     @Override
-    protected MapCodec<? extends BlockWithEntity> getCodec() {
-        return null;
+    protected MapCodec<? extends MeatStillBlock> getCodec() {
+        return CODEC;
     }
 
     @Override
