d042ff9b0028abfd7a08ae678bd6fbe2b67d7578
TheDeathlyCow <53499406+TheDeathlyCow@users.noreply.github.com>
2025-09-30 22:45:21 +1300

    fix water bucket pollutant collection

--- src/main/java/com/thedeathlycow/thirstful/thirst/WaterPollution.java
+++ src/main/java/com/thedeathlycow/thirstful/thirst/WaterPollution.java
@@ -1,47 +1,20 @@
 package com.thedeathlycow.thirstful.thirst;
 
-import com.thedeathlycow.thirstful.Thirstful;
-import com.thedeathlycow.thirstful.config.common.WaterPollutionConfig;
 import com.thedeathlycow.thirstful.item.component.PollutantComponent;
 import com.thedeathlycow.thirstful.registry.tag.TBiomeTags;
 import com.thedeathlycow.thirstful.registry.tag.TItemTags;
-import net.fabricmc.fabric.api.lookup.v1.block.BlockApiLookup;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
-import org.jetbrains.annotations.Nullable;
 
 public final class WaterPollution {
-    public static final PollutantComponent FALLBACK = new PollutantComponent(true, true, false);
-
-    public static final BlockApiLookup<PollutantComponent, Void> POLLUTANT_CONTAINER = BlockApiLookup.get(
-            Thirstful.id("water_pollutants"),
-            PollutantComponent.class,
-            Void.class
-    );
-
-    public static void initialize() {
-        Thirstful.LOGGER.debug("Initialized Thirstful pollutant lookup API");
-        POLLUTANT_CONTAINER.registerForBlocks(WaterPollution::waterSourceLookup, Blocks.WATER);
-        POLLUTANT_CONTAINER.registerFallback((world, pos, state, blockEntity, context) -> FALLBACK);
-    }
-
     public static boolean canCarryPollutants(ItemStack stack) {
         return stack.isIn(TItemTags.CAN_BE_POLLUTED) && !stack.isIn(TItemTags.CAN_NOT_BE_POLLUTED);
     }
 
-    private static PollutantComponent waterSourceLookup(
-            World world,
-            BlockPos pos,
-            BlockState state,
-            @Nullable BlockEntity blockEntity,
-            Void context
-    ) {
+    public static PollutantComponent findPollutants(World world, BlockPos pos) {
         boolean dirty = true;
         boolean contaminated = true;
         boolean salty = false;
