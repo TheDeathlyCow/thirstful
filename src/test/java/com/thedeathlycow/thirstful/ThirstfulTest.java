b53e31c23e13ca8636aa0ad1a778999e5a9b09e6
TheDeathlyCow <thedeathlycow@gmail.com>
2026-05-01 18:38:13 -1000

    migrate test sources to mojmap

--- src/test/java/com/thedeathlycow/thirstful/ThirstfulTest.java
+++ src/test/java/com/thedeathlycow/thirstful/ThirstfulTest.java
@@ -1,13 +1,13 @@
 package com.thedeathlycow.thirstful;
 
-import net.minecraft.Bootstrap;
 import net.minecraft.SharedConstants;
+import net.minecraft.server.Bootstrap;
 
 public final class ThirstfulTest {
 
     public static void initialize() {
-        SharedConstants.createGameVersion();
-        Bootstrap.initialize();
+        SharedConstants.tryDetectVersion();
+        Bootstrap.bootStrap();
         Thirstful.initialize();
     }
 
